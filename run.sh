8e7725551ae942c604887214b13326e008b3ec23
Maksim1Bomba <eeggorr120207@outlook.com>
2025-03-08 13:42:22 +0300

    fix dockerfile from services, copy all requirement frontend files

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker run --rm -it --name server --network net -p 8080:8080 server;
 docker run --rm -it --name nginx --network net -p 80:80 -p 443:443 nginx;
+docker run --rm -it --name server --network net -p 8080:8080 server;
