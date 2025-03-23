1cf76efa6da9d9f5033aec69bf9c58092370d102
Maksim1Bomba <eeggorr120207@outlook.com>
2025-03-23 18:18:59 +0300

    fix build.sh and renew_cert.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 docker volume create hs1;
 
+sudo certbot certonly --standalone  -d your_domain_name --config-dir /your/path/to/cert --email your_email --agree-tos --force-renewal;
+sudo chown -R your_name:your_name ./services/nginx/cert;
+
+
 docker build -t nginx ./services/nginx/;
 docker build -t server ./backend;
 
 sudo docker network create -d bridge net;
-
-sudo certbot certonly --standalone  -d your_domain_name --config-dir ./services/nginx/cert/ --email your_email --agree-tos --force-renewal
